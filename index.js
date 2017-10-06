53477b8f619642df62fc6c3c9b1986d64d4fffd9
Ryan <rrichardson610@gmail.com>
2017-10-06 10:27:00 -0600

    boilerplate aaannnndd a button that pulls up the modal

---
+++ index.js
@@ -0,0 +1,6 @@
+let modalButton = document.getElementById('showModal');
+let modal = document.getElementById('theModal');
+
+modalButton.addEventListener('click', function() {
+	modal.style.display = 'block';
+});
