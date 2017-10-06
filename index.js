ff6bf5a45647f6b743686d9aa0630ae4fdfaebfe
Ryan <rrichardson610@gmail.com>
2017-10-06 10:56:18 -0600

    created working modal close button and timer for popup

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 let modalButton = document.getElementById('showModal');
 let modal = document.getElementById('theModal');
+let modalClose = document.getElementById('modalClose');
 
 modalButton.addEventListener('click', function() {
 	modal.style.display = 'block';
 });
+
+modalClose.addEventListener('click', function() {
+	modal.style.display = 'none';
+});
+
+
+setTimeout(function(){
+  modal.style.display = 'block';
+}, 5000);
