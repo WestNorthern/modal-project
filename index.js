93f1aa408739cc8bf810aa699b8acdb0efd2f903
Ryan <rrichardson610@gmail.com>
2017-10-06 13:46:14 -0600

    changed all of the content, added new theme

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-let modalButton = document.getElementById('showModal');
+// let modalButton = document.getElementById('showModal');
 let modal = document.getElementById('theModal');
 let modalClose = document.getElementById('modalClose');
 
-modalButton.addEventListener('click', function() {
-	modal.style.display = 'block';
-});
+// modalButton.addEventListener('click', function() {
+// 	modal.style.display = 'block';
+// });
 
 modalClose.addEventListener('click', function() {
 	modal.style.display = 'none';
@@ -15,8 +15,3 @@ setTimeout(function(){
   modal.style.display = 'block';
 }, 5000);
 
-// document.getElementById('body').addEventListener('click', function() {
-// 	if (click.target !== '#theModal'){
-// 	modal.style.display = 'none';
-// }
-// });
